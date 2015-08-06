b55c7b4012dba2476ded76e40b3c6009dc7101f9
KyleRattet <krattet@gmail.com>
2015-08-06 16:27:51 -0600

    solution to problem 1 added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // add scripts
 
-console.log("sanity check!");
+function largestNumber (array) {
+  var largest = array[0];
+
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] > largest) {
+      largest = array[i];
+    }
+  }
+  return largest;
+}
+
+
+module.exports = {
+  largestNumber: largestNumber
+}
--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,10 @@
 var code = require('../js/main.js');
 
 
-// describe('', function() {
-//   it('', function() {
-
-//   });
-// });
+//Problem 1
+describe('Problem 1', function() {
+  it('should return the largest number', function() {
+    expect(code.largestNumber([10, 20, 30])).toBe(30);
+    expect(code.largestNumber([10, 20, 40])).toBe(40);
+  });
+});
