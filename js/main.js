1e77370f20a4a363b2b082676a307ed3d19976d9
KyleRattet <krattet@gmail.com>
2015-08-07 08:43:03 -0600

    problem 2 function and test are complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // add scripts
 
+//PROBLEM 1
 function largestNumber (array) {
   var largest = array[0];
 
@@ -11,7 +12,26 @@ function largestNumber (array) {
   return largest;
 }
 
+//PROBLEM 2
+function oddNumber (number) {
+  var odd = [];
+
+  if(number > 100 || number < 0) {
+    return "number has to be between 0 and 100";
+  }
+  if (number <= 40){
+    return number;
+  } else {
+  for (var i = 40; i <= number; i++) {
+    if(i%2 === 1) {
+      odd.push(i);
+      }
+    }
+  }
+  return odd;
+}
 
 module.exports = {
-  largestNumber: largestNumber
+  largestNumber: largestNumber,
+  oddNumber: oddNumber
 }
--- spec/spec.js
+++ spec/spec.js
@@ -1,10 +1,19 @@
 var code = require('../js/main.js');
 
 
-//Problem 1
+//PROBLEM 1
 describe('Problem 1', function() {
   it('should return the largest number', function() {
     expect(code.largestNumber([10, 20, 30])).toBe(30);
     expect(code.largestNumber([10, 20, 40])).toBe(40);
+  });
+});
+
+//PROBLEM 2
+describe('Problem 2', function() {
+  it('should take a random number from 0 to 100, returns odd numbers from 40 to the random number', function() {
+    expect(code.oddNumber(48)).toEqual([41,43,45,47]);
+    expect(code.oddNumber(105)).toEqual("number has to be between 0 and 100");
+    expect(code.oddNumber(30)).toEqual(30);
   });
 });
